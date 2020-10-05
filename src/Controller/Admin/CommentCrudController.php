667416dfe7e400f66364b2ab140fd4b3c688ff66
Aurelie-feyzin <gramusset.a@gmail.com>
2020-10-05 11:07:24 +0200

    First Admin Backend

---
+++ src/Controller/Admin/CommentCrudController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CommentCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Comment::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            AssociationField::new('conference'),
+            TextField::new('author'),
+            EmailField::new('email'),
+            TextEditorField::new('text'),
+            DateField::new('createdAt')->setFormat('short'),
+            TextField::new('photoFilename')
+        ];
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('conference');
+    }
+
+}
---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+
+        return $this->redirect($routeBuilder->setController(ConferenceCrudController::class)->generateUrl());
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Guestbook');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoRoute('Back to the website', 'fa fa-home', 'homepage');
+        yield MenuItem::linkToCrud('Conferences', 'fa fa-map-marker', Conference::class);
+        yield MenuItem::linkToCrud('Comments', 'fa fa-comments', Comment::class)
+            ->setDefaultSort(['createdAt' => 'ASC']);
+        ;
+
+
+
+    }
+}
