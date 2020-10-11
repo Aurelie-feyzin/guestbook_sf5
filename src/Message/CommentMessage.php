b0fdce5b3cc70488574eec4307d1aa4c3f590425
Aurelie-feyzin <gramusset.a@gmail.com>
2020-10-11 15:07:42 +0200

    Feature add notification to Slack for new Comment with "awesome|great" in new comment

--- src/Message/CommentMessage.php
+++ src/Message/CommentMessage.php
@@ -7,18 +7,25 @@ namespace App\Message;
 class CommentMessage
 {
     private $id;
-
+    private $reviewUrl;
     private $context;
 
-    public function __construct(int $id, array $context = [])
+
+    public function __construct(int $id, string $reviewUrl, array $context = [])
     {
         $this->id = $id;
+        $this->reviewUrl = $reviewUrl;
         $this->context = $context;
     }
 
     public function getId()
     {
         return $this->id;
+    }
+
+    public function getReviewUrl()
+    {
+        return $this->reviewUrl;
     }
 
     public function getContext()
--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -69,7 +69,8 @@ class CommentMessageHandler implements MessageHandlerInterface
             $this->entityManager->flush();
             $this->bus->dispatch($message);
         } elseif ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
-            $this->notifier->send(New CommentReviewNotification($comment), ...$this->notifier->getAdminRecipients());
+            $notification = new CommentReviewNotification($comment, $message->getReviewUrl());
+            $this->notifier->send($notification, ...$this->notifier->getAdminRecipients());
         } elseif ($this->workflow->can($comment, 'optimize')) {
             if ($comment->getPhotoFilename()) {
                 $this->imageOptimizer->resize($this->photoDir . '/' . $comment->getPhotoFilename());
